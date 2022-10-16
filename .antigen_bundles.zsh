2172828dd703737149be4ac8123c7e307d9c53ef
gignsky <gignsky@gmail.com>
2022-10-16 01:48:07 -0400

    moved antigen bundles into their own dir

---
+++ .antigen_bundles.zsh
@@ -0,0 +1,12 @@
+antigen bundle git
+antigen bundle pip
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle cp
+antigen bundle sudo
+antigen bundle colored-man-pages
+antigen bundle command-not-found
+antigen bundle djui/alias-tips
+antigen bundle aliases
+antigen bundle ansible
