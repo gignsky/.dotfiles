8fa19a656f9dbf6f87d79c10e8428a70f87fcf74
gignsky <gignsky@gmail.com>
2022-10-27 03:34:54 -0400

    updated antigen bundles

--- .antigen_bundles.zsh
+++ .antigen_bundles.zsh
@@ -1,13 +1,9 @@
 antigen bundle git
 antigen bundle pip
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle zsh-users/zsh-completions
 antigen bundle cp
 antigen bundle sudo
 antigen bundle colored-man-pages
 antigen bundle command-not-found
-antigen bundle djui/alias-tips
 antigen bundle aliases
 antigen bundle ansible
 antigen bundle virtualenvwrapper
---
+++ .antigen_bundles_2.zsh
@@ -0,0 +1,4 @@
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle djui/alias-tips
