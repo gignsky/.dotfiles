8567699518e7d5dbbe64747f8201395bdcaba089
gignsky <gignsky@gmail.com>
2022-11-10 13:05:52 -0500

    added some new zsh plugins

--- .antigen_bundles_2.zsh
+++ .antigen_bundles_2.zsh
@@ -2,3 +2,5 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle djui/alias-tips
+antigen bundle zpm-zsh/ls
+antigen bundle zpm-zsh/colors
