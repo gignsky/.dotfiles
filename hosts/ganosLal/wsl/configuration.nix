45f876e24f9b1fbce40ff0366a17f891328981ec
gignsky <gignsky@gmail.com>
2024-08-26 05:14:26 +0000

    chore: Update flake.lock and flake.nix files

--- hosts/ganosLal/wsl/configuration.nix
+++ hosts/ganosLal/wsl/configuration.nix
@@ -1,1 +1,43 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "gig";
+
+  environment.systemPackages = [
+    pkgs.wget
+    pkgs.git
+    pkgs.bat
+    pkgs.tree
+  ];
+
+  # programs.nix-ld = {
+  #   enable = true;
+  #   package = pkgs.nix-ld-rs;
+  # };
+
+  # Alternative that doesn't effect other files
+  vscode-remote-workaround.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
