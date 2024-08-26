499157ff2e4c72bcd974d644edaa8c2bb039060b
gignsky <gignsky@gmail.com>
2024-08-26 10:59:02 -0400

    started home-manager use

--- flake.nix
+++ flake.nix
@@ -122,10 +122,10 @@
       homeConfigurations = {
         # FIXME replace with your username@hostname
         "gig@nixos" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {inherit inputs outputs;};
+          inherit pkgs; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = {inherit specialArgs;};
           # > Our main home-manager configuration file <
-          modules = [./home-manager/home.nix];
+          modules = [./home/home.nix];
         };
       };
   };
--- home/gig/home.nix
+++ home/gig/home.nix
@@ -24,9 +24,9 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+      # outputs.overlays.additions
+      # outputs.overlays.modifications
+      # outputs.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
@@ -47,8 +47,8 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "gig";
+    homeDirectory = "/home/gig";
   };
 
   # Add stuff for your user as you see fit:
@@ -63,5 +63,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.05";
 }
