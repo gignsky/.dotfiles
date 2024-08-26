81e0c990270e8c0feb6bd363b92e7a31987922d9
gignsky <gignsky@gmail.com>
2024-08-26 11:52:55 -0400

    chore: Update home-manager configuration for gig

--- home/gig/home.nix
+++ home/gig/home.nix
@@ -10,14 +10,10 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
+    # ./common/core
   ];
 
   nixpkgs = {
@@ -45,7 +41,6 @@
     };
   };
 
-  # TODO: Set your username
   home = {
     username = "gig";
     homeDirectory = "/home/gig";
@@ -53,7 +48,16 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    wget
+    cowsay
+    figlet
+    lolcat
+    bat
+    tree
+    magic-wormhole
+    just
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
