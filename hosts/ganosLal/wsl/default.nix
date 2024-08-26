4d558f6737129d3f159a925249635090d3287a8a
gignsky <gignsky@gmail.com>
2024-08-26 05:20:30 +0000

    feat: Add WSL configuration for ganosLal

---
+++ hosts/ganosLal/wsl/default.nix
@@ -0,0 +1,29 @@
+{ inputs, lib, config, pkgs, ...}: {
+  imports = [
+    ../common/core
+    ../common/users/gig
+  ];
+
+  networking.hostName = "wsl-ganosLal";
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+
+    };
+
+  # fixes vscode remote wsl stuff
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
+
+    channel.enable = false;
+
+    system.stateVersion = "24.05";
+}
