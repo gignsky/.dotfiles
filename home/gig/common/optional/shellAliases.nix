b4c7f1379dd21234f427917c6b5a820569ada246
gignsky <gignsky@gmail.com>
2024-08-28 18:07:17 -0400

    added shell aliases

---
+++ home/gig/common/optional/shellAliases.nix
@@ -0,0 +1,14 @@
+{...}: {
+  ll = "ls -lh";
+  lla = "ls -lah";
+  cp = "cp -rv";
+  mv = "mv -v";
+  rd = "rmdir";
+  rdd = "rm -rfv";
+  cls = "clear";
+  md = "mkdir";
+  als = "alias";
+  syst = "systemctl";
+  expo = "export NIXPKGS_ALLOW_UNFREE=1";
+  cat = "bat";
+}
