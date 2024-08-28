6d3552990ad974215856c156a9a9c9b7b6c46959
gignsky <gignsky@gmail.com>
2024-08-28 15:16:16 -0400

    chore: Update system-wide packages and remove unused packages

--- hosts/ganosLal/nixos-wsl/default.nix
+++ hosts/ganosLal/nixos-wsl/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../common/core
     ../../common/users/gig
+    # inputs.nixos-wsl.modules
     inputs.home-manager.nixosModules.home-manager
   ];
 
@@ -12,6 +13,9 @@
     enable = true;
     package = pkgs.nix-ld-rs;
   };
+
+  wsl.enable = true;
+  wsl.defaultUser = "gig";
 
   nix = let
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
@@ -28,30 +32,6 @@
 
   };
 
-
-  # unstable overlay
-  nixpkgs.overlays = [
-    (final: prev: {
-      unstable = import inputs.nixpkgs-unstable {
-        system = final.system;
-        config.allowUnfree = true;
-      };
-    })
-  ];
-
-  # System-wide packages installed by root
-  environment.systemPackages = with pkgs; [
-    wget
-    bat
-    tree
-    magic-wormhole
-    git
-
-    # unstable packages
-    # just
-    unstable.just # need unstable for latest version
-  ];
-
 #   # I think this is unneccecary if I'm going with standalone home-manager rather than flake os module home-manager
 #   home-manager = {
 #     extraSpecialArgs = { inherit inputs outputs; };
@@ -60,6 +40,8 @@
 #       gig = import ../../../home/gig/ganosLal/wsl.nix;
 #     };
 #   };
+
+  # nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "24.05";
 }
