3b79fe7831fc2dca3d4f51283c69b9040948b237
gignsky <gignsky@gmail.com>
2024-08-28 18:06:54 -0400

    reconfigured packages

--- home/gig/common/core/default.nix
+++ home/gig/common/core/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
     # imports = [
@@ -21,7 +21,7 @@
         stateVersion = "24.05";
         sessionPath = []; # Add paths to $PATH
         sessionVariables = {
-            FLAKE = "$HOME/src/nix-config";
+            FLAKE = "$HOME/.dotfiles/.";
             SHELL = "zsh";
             # TERM = "kitty";
             # TERMINAL = "kitty";
@@ -30,32 +30,12 @@
         };
     };
 
-    home.packages = builtins.attrValues {
-        inherit (pkgs)
-            # borgbackup
-            # eza
-            # fd
-            # findutils
-            # jq
-            # nix-tree
-            # ncdu
-            # pciutils
-            # pfetch
-            # p7zip
-            # ripgrep
-            # usbutils
-            # unzip
-            # unrar
-            # zip;
-            btop
-            # vscode
-            coreutils
-            curl
-            fzf
-            pre-commit
-            tree
-            wget;
-    };
+    # home.packages = builtins.attrValues {
+    #     inherit (pkgs)
+    #         btop
+    #         coreutils
+    #         fzf;
+    # };
 
     nixpkgs.config.allowUnfree = true;
 
