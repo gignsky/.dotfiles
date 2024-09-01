5642047096ae18789194b7bcbd35ae952b89522b
gignsky <gignsky@gmail.com>
2024-08-31 21:20:06 -0400

    removed redundency

--- home/gig/home.nix
+++ home/gig/home.nix
@@ -79,11 +79,6 @@
 #     };
 #   };
 
-  home = {
-    username = "gig";
-    homeDirectory = "/home/gig";
-  };
-
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   home.packages = with pkgs; [];
