a6de067d4d507dde068cc1237e81c4d79d6e07d4
gignsky <gignsky@gmail.com>
2024-09-01 08:24:46 -0400

    added inital als package

---
+++ pkgs/default.nix
@@ -0,0 +1,11 @@
+# You can build these directly using 'nix build .#example'
+
+{
+  pkgs ? import <nixpkgs> { },
+}:
+rec {
+
+  #################### Packages with external source ####################
+
+  als = pkgs.callPackage ./als.nix { };
+}
