c0f510fef0514f49840c4c62288cd93ee2b743b6
gignsky <gignsky@gmail.com>
2024-08-31 23:17:08 -0400

    got zsh working

--- hosts/common/optional/zsh.nix
+++ hosts/common/optional/zsh.nix
@@ -2,14 +2,15 @@
 
 {
   users.defaultUserShell = pkgs.zsh;
+  users.users.gig.shell = pkgs.zsh;
   environment.shells = [ pkgs.zsh ];
 
   programs.zsh = {
     enable = true;
 
-    history = {
-      size = lib.mkDefault 10000;
-      path = lib.mkDefault "/home/gig/.zsh_history";
-    };
+    # history = {
+    #   size = lib.mkDefault 10000;
+    #   path = lib.mkDefault "/home/gig/.zsh_history";
+    # };
   };
 }
