be56230960dcf3cebe5f6c0eccaba59a8a6252e3
gignsky <gignsky@gmail.com>
2024-09-01 10:21:48 -0400

    customized flake build sh

--- scripts/flake-build.sh
+++ scripts/flake-build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 if [ ! -z $1 ]; then
-	export PACKAGE=$1
+	if [ $1 == "file" ]; then
+		export PATH_USED=TRUE
+		export PATH=$(pwd)/$2
+	else
+		export PACKAGE=$1
+	fi
 else
+	export PATH_USED=FALSE
 	export PACKAGE=""
 fi
 
-nix build .#$PACKAGE
+if [ $PATH_USED == TRUE ]; then
+	nix build --file $PATH
+else
+	nix build .#$PACKAGE
+fi
