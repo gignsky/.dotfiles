dee470c607aae77338058e1fe23a0926940c7421
gignsky <gignsky@gmail.com>
2024-09-09 22:43:56 -0400

    trying to get sops working at the home level

--- hosts/common/users/gig.nix
+++ hosts/common/users/gig.nix
@@ -1,6 +1,7 @@
 { pkgs, config, ... }:
 
 {
+  # decrypt gig-password to /run/secrets-for-users/ so it can be used to create the user
   sops.secrets.gig-password.neededForUsers = true;
   users.mutableUsers = false; # required for password to be set via sops during system activation
 
@@ -13,30 +14,17 @@
   # users.mutableUsers = false; # required for password to be set via sops during system activation
 
   users.users.gig = {
-    # hashedPasswordFile = config.sops.secrets.gig-password.path;
     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
     # Be sure to change it (using passwd) after rebooting!
     # initialPassword = "correcthorsebatterystaple";
     isNormalUser = true;
-    # hashedPassword = config.sops.secrets.gig-password.path;
+    hashedPasswordFile = config.sops.secrets.gig-password.path;
     openssh.authorizedKeys.keys = [
     ];
     # shell = pkgs.zsh; #default shell
     extraGroups = [
       "wheel"
       "gig"
-    ];
-
-
-  };
-
-  users.users.tester = {
-    isNormalUser = true;
-    hashedPasswordFile = config.sops.secrets.gig-password.path;
-    openssh.authorizedKeys.keys = [
-    ];
-    extraGroups = [
-      "wheel"
     ];
   };
 
