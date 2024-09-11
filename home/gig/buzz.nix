936515daea1b1c1555fc9b79054af8689a583604
gignsky <gignsky@gmail.com>
2024-09-10 23:23:05 -0400

    Refactor buzz configuration to use home.nix instead of common/core

--- home/gig/buzz.nix
+++ home/gig/buzz.nix
@@ -15,7 +15,7 @@
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
     #################### Required Configs ####################
-    ./common/core #required
+    ./home.nix #required
 
     #################### Host-specific Optional Configs ####################
 
