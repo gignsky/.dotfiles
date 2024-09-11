94c28e57b108d9d5e0567935b4f5417261439402
gignsky <gignsky@gmail.com>
2024-09-10 23:09:37 -0400

    Enable QEMU guest services and update system state version

--- hosts/buzz/default.nix
+++ hosts/buzz/default.nix
@@ -56,6 +56,8 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
+  services.qemuGuest.enable = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
