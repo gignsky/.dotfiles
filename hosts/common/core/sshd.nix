33ba210006563e96d66e4ba07029507dfd35f327
gignsky <gignsky@gmail.com>
2024-09-10 23:26:28 -0400

    Refactor SSH server configuration to enable root login

--- hosts/common/core/sshd.nix
+++ hosts/common/core/sshd.nix
@@ -4,7 +4,7 @@
     # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = lib.mkDefault false;
+    enable = lib.mkDefault true;
     settings = {
       # Opinionated: forbid root login through SSH.
       PermitRootLogin = lib.mkDefault "no";
