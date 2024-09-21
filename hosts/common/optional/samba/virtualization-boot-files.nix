3d8ed85d47a7f52ec1889b749e248692389b60e0
gignsky <gignsky@gmail.com>
2024-09-20 21:29:44 -0400

    added virtualization boot files to samba share

---
+++ hosts/common/optional/samba/virtualization-boot-files.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  fileSystems."/home/gig/virtualization-boot-files" = {
+    device = "//192.168.51.3/virtualization-boot-files";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      creds = "/etc/samba/cifs-creds";
+      uid = "1000";
+      gid = "100";
+    in
+    ["${automount_opts},credentials=${creds},uid=${uid},gid=${gid},vers=3.0"];
+  };
+}
