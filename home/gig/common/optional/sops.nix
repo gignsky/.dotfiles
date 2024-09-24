daed533812455694177cce2027f34670df75735a
gignsky <gignsky@gmail.com>
2024-09-23 22:35:45 -0400

    updated sops.nix

--- home/gig/common/optional/sops.nix
+++ home/gig/common/optional/sops.nix
@@ -1,5 +1,10 @@
-{ inputs, configLib, ...}:
+{ inputs, config, configLib, ...}:
 
+let
+  # secretsDirectory = builtins.toString inputs.nix-secrets;
+  # secretsFile = "{secretsDirectory}/secrets.yaml";
+  homeDirectory = config.home.homeDirectory;
+in
 {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
@@ -7,13 +12,23 @@
 
   sops = {
     # This is the gig/dev key and needs ot have been copied to this location on the host
-    age.keyFile = "/home/gig/.config/sops/age/keys.txt";
+    # age.keyFile = "/home/gig/.config/sops/age/keys.txt";
+    # age = {
+    #   # automatically import host SSH keys as age keys
+    #   sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    #   #this will use an age key that is expected to already be in the filesystem
+    #   keyFile = "/var/lib/sops-nix/key.txt";
+    #   # generate a new key if the key specified above doesn't exist
+    #   generateKey = true;
+    # };
+
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFile = (configLib.relativeToRoot "./secrets.yaml");
     validateSopsFiles = false;
 
     secrets = {
       "private_keys/dev" = {
-        path = "/home/gig/.ssh/id_rsa";
+        path = "${homeDirectory}/.ssh/id_rsa";
       };
     };
 
