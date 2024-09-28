22b36449be37ba2e8319700db874c8583e8a1a83
gignsky <gignsky@gmail.com>
2024-09-28 18:46:53 -0400

    Refactor example configs submodule and remove unnecessary files

--- resources/example-configs/nix-config-tmp/home/ta/common/core/direnv.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    nix-direnv.enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
-  };
-}
--- resources/example-configs/nix-config-tmp/modules/nixos/default.nix
+++
@@ -1,7 +0,0 @@
-# Add your reusable NixOS modules to this directory, on their own file (https://wiki.nixos.org/wiki/NixOS_modules).
-# These should be stuff you would like to share with others, not your personal configurations.
-
-{ configLib, ... }:
-{
-  imports = configLib.scanPaths ./.;
-}
