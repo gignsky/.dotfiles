166fd2ec49142da4de9bc30c69ed74354bd2e980
testgig <testgig@gignsky.com>
2024-11-21 19:44:07 -0500

    refactor git.nix configuration for consistency

--- home/gig/common/core/git.nix
+++ home/gig/common/core/git.nix
@@ -4,11 +4,11 @@
     programs.git = {
         enable = true;
         extraConfig = {
-        user.useConfigOnly = true;
-        user.name = "gignsky";
-        user.email = "gig@gignsky.com";
-        pull.rebase = true;
-        # pull.ff = "only";
+            user.useConfigOnly = true;
+            user.name = "gignsky";
+            user.email = "gig@gignsky.com";
+            pull.rebase = true;
+            # pull.ff = "only";
         };
     };
 }
