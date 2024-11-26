5c3f2c894c9cf7a9d5fa18167f0ec293d28e7d54
gignsky <gig@gignsky.com>
2024-11-26 02:12:04 -0500

    added a bunch of options to generic-extensions.nix

--- home/gig/common/optional/vscode/generic-extensions.nix
+++ home/gig/common/optional/vscode/generic-extensions.nix
@@ -6,12 +6,26 @@
     ##############################################
 
     # Home Manager Way
-    programs.vscode = {
-        enable = true;
-        extensions = with pkgs.vscode-extensions; [
-            # bbenoist
-            # nix
-            ms-vscode-remote.remote-ssh
-        ];
+    programs = {
+        vscode = {
+            enable = true;
+            enableExtensionUpdateCheck = true;
+            enableUpdateCheck = true;
+            mutableExtensionsDir = false; # disallows vscode from installing its own extensions
+            # copy and paste user settings into the field below
+            userSettings = {};
+
+            extensions = with pkgs.vscode-extensions; [
+                # bbenoist
+                # nix
+                ms-vscode-remote.remote-ssh
+            ];
+        };
+
+        # A "wonderful CLI to track your time"
+        watson = {
+            enable = false;
+            enableZshIntegration = true;
+        };
     };
 }
