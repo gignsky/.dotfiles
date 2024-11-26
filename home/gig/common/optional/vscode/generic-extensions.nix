db9100241ece7f944e18a9e7a3752259fe1566af
gignsky <gig@gignsky.com>
2024-11-26 02:05:12 -0500

    added basic vscode options for vscode into home-manager config

--- home/gig/common/optional/vscode/generic-extensions.nix
+++ home/gig/common/optional/vscode/generic-extensions.nix
@@ -4,21 +4,14 @@
     # info at
     # https://nixos.wiki/wiki/Visual_Studio_Code
     ##############################################
-    
-    home.packages = with pkgs; [
-	  (vscode-with-extensions.override {
-        vscodeExtensions = with vscode-extensions; [
-            bbenoist.nix # nix language package
-            # ms-python.python # python extension
+
+    # Home Manager Way
+    programs.vscode = {
+        enable = true;
+        extensions = with pkgs.vscode-extensions; [
+            # bbenoist
+            # nix
             ms-vscode-remote.remote-ssh
-        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-            # {
-            #     name = "remote-ssh-edit";
-            #     publisher = "ms-vscode-remote";
-            #     version = "0.47.2";
-            #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-            # }
         ];
-      })
-    ];
+    };
 }
