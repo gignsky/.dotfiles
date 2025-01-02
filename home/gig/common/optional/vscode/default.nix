3009564cb074834fea25599ef8cb585f2ccdb274
gignsky <gig@gignsky.com>
2025-01-01 22:27:57 -0500

    add WSL support for VSCode configuration and create separate default.nix files for WSL and regular environments

--- home/gig/common/optional/vscode/default.nix
+++ home/gig/common/optional/vscode/default.nix
@@ -1,26 +1,11 @@
-{ inputs, outputs, configLib, pkgs, ... }:
+{ inputs, outputs, configLib, pkgs, lib, ... }:
 
 {
-    imports = (configLib.scanPaths ./.);
+  options.vscodeIsWSL = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Set to true if running in WSL.";
+  };
 
-    #home.packages = with pkgs; [
-	 # vscode
-    #];
-
-    # home.packages = with pkgs; [
-	#   (vscode-with-extensions.override {
-    #     vscodeExtensions = with vscode-extensions; [
-    #         bbenoist.nix # nix language package
-    #         # ms-python.python # python extension
-    #         ms-vscode-remote.remote-ssh
-    #     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    #         # {
-    #         #     name = "remote-ssh-edit";
-    #         #     publisher = "ms-vscode-remote";
-    #         #     version = "0.47.2";
-    #         #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-    #         # }
-    #     ];
-    #   })
-    # ];
+  imports = config: if config.vscodeIsWSL then ./wsl else ./regular;
 }
---
+++ home/gig/common/optional/vscode/wsl/generic-extensions.nix
@@ -0,0 +1,37 @@
+{ inputs, outputs, pkgs, ... }:
+
+{
+    # info at
+    # https://nixos.wiki/wiki/Visual_Studio_Code
+    ##############################################
+
+    # search extentions at
+    # https://search.nixos.org/packages?type=packages&query=vscode-extensions
+    ##############################################
+
+    # Home Manager Way
+    programs = {
+        vscode = {
+            enable = false;
+            enableExtensionUpdateCheck = true;
+            enableUpdateCheck = true;
+            mutableExtensionsDir = false; # disallows vscode from installing its own extensions
+            # copy and paste user settings into the field below
+            userSettings = {};
+
+            extensions = with pkgs.vscode-extensions; [
+                bbenoist.nix
+                oderwat.indent-rainbow
+                ms-vscode-remote.remote-ssh
+                github.copilot
+                github.copilot-chat
+            ];
+        };
+
+        # # A "wonderful CLI to track your time"
+        # watson = {
+        #     enable = false;
+        #     enableZshIntegration = true;
+        # };
+    };
+}
