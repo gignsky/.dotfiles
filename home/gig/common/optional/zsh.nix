a3c58e94d202880157aba8f4c82b50902fc5aa6b
gignsky <gig@gignsky.com>
2025-01-03 17:45:51 -0500

    added new zsh plugins

--- home/gig/common/optional/zsh.nix
+++ home/gig/common/optional/zsh.nix
@@ -56,9 +56,9 @@
         # { name = "zsh-users/zsh-autosuggestions";}
         # { name = "zsh-users/zsh-completions";}
         # { name = "djui/alias-tips";}
-        # { name = "zpm-zsh/ls";}
-        # { name = "zpm-zsh/colors";}
-        # { name = "zpm-zsh/dircolors-neutral";}
+        { name = "zpm-zsh/ls";}
+        { name = "zpm-zsh/colors";}
+        { name = "zpm-zsh/dircolors-neutral";}
         # { name = "dbz/kube-aliases";}
         # { name = "zsh-users"; plugin = "zsh-completions"; }
         # { name = "zsh-users"; plugin = "zsh-syntax-highlighting"; }
