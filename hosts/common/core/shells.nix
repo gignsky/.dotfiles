f81c9990b94ceb08c3c276945cddd508d98a146d
gignsky <gig@gignsky.com>
2025-04-15 00:34:45 -0400

    updated nixpkgs

--- hosts/common/core/shells.nix
+++ hosts/common/core/shells.nix
@@ -1,87 +1,89 @@
 { pkgs, outputs, configLib, ... }:
 
 {
-    # unstable overlay
-    nixpkgs.overlays = [ outputs.overlays.unstable-packages ];
+  # unstable overlay
+  nixpkgs.overlays = [ outputs.overlays.unstable-packages ];
 
 
-    environment.systemPackages = with pkgs; [
-        tree
-        btop
-        bat
-        tree
-        git
-        gedit
-        magic-wormhole
-        wget
-        screen
-        neofetch
+  environment.systemPackages = with pkgs; [
+    tree
+    btop
+    bat
+    tree
+    git
+    gedit
+    magic-wormhole
+    wget
+    screen
+    neofetch
 
-        # nixos-unstable packages
-        unstable.just
-    ];
+    # nixos-unstable packages
+    unstable.just
+  ];
 
-    # # Direnv
-    # programs.direnv = {
-    #     enable = lib.mkDefault true;
-    #     # Expanded settings:
-    #     nix-direnv = {
-    #         enable = true;
-    #         package = pkgs.nix-direnv;
-    #     };
-    #     silent = true;
-    #     loadInNixShell = true;
-    #     # direnvrcExtra = lib.mkDefault ''
-    #     #     echo "direnv: loading direnvrc"
-    #     # '';
-    # };
+  # # Direnv
+  # programs.direnv = {
+  #     enable = lib.mkDefault true;
+  #     # Expanded settings:
+  #     nix-direnv = {
+  #         enable = true;
+  #         package = pkgs.nix-direnv;
+  #     };
+  #     silent = true;
+  #     loadInNixShell = true;
+  #     # direnvrcExtra = lib.mkDefault ''
+  #     #     echo "direnv: loading direnvrc"
+  #     # '';
+  # };
 
-    programs.direnv = {
-    	enable = true;
-   	    package = pkgs.direnv;
-        silent = false;
-        loadInNixShell = true;
-        direnvrcExtra = "";
-        nix-direnv = {
-            enable = true;
-            package = pkgs.nix-direnv;
-        };
+  programs.direnv = {
+    enable = true;
+    package = pkgs.direnv;
+    silent = false;
+    loadInNixShell = true;
+    direnvrcExtra = "";
+    nix-direnv = {
+      enable = true;
+      package = pkgs.nix-direnv;
+    };
+  };
+
+  programs.bash =
+    let
+      sword = (configLib.relativeToRoot "resources/sword.art");
+    in
+    {
+      # enable = true;
+      completion.enable = true;
+      enableLsColors = true;
+      shellAliases = {
+        ll = "ls -lh";
+        lla = "ls -lah";
+        cp = "cp -rv";
+        mv = "mv -v";
+        rd = "rmdir";
+        rdd = "rm -rfv";
+        cls = "clear";
+        md = "mkdir";
+        # als = "alias";
+        syst = "systemctl";
+        expo = "export NIXPKGS_ALLOW_UNFREE=1";
+        cat = "bat";
+        dot = "cd ~/.dotfiles";
+      };
+
+      shellInit = ''
+        cat ${sword} | ${pkgs.lolcat}/bin/lolcat
+      '';
+
+      # loginShellInit = ''
+      #     Shell script code called during login bash shell init
+      # '';
     };
 
-    programs.bash = let
-        sword = (configLib.relativeToRoot "resources/sword.art");
-    in {
-        # enable = true;
-        enableCompletion = true;
-        enableLsColors = true;
-        shellAliases = {
-            ll = "ls -lh";
-            lla = "ls -lah";
-            cp = "cp -rv";
-            mv = "mv -v";
-            rd = "rmdir";
-            rdd = "rm -rfv";
-            cls = "clear";
-            md = "mkdir";
-            # als = "alias";
-            syst = "systemctl";
-            expo = "export NIXPKGS_ALLOW_UNFREE=1";
-            cat = "bat";
-            dot = "cd ~/.dotfiles";
-        };
-
-        shellInit = ''
-            cat ${sword} | ${pkgs.lolcat}/bin/lolcat
-        '';
-
-        # loginShellInit = ''
-        #     Shell script code called during login bash shell init
-        # '';
-    };
-
-    # Most basic zsh install just so it's already on the system
-    programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-    };
+  # Most basic zsh install just so it's already on the system
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
 }
