a533c38b25a9b3c46398ce7a4a70d23d17487de3
gignsky <gig@gignsky.com>
2025-04-17 07:44:48 -0400

    updated bootstrapper

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
 # This file defines overlays/custom modifications to upstream packages
 #
 
-{ inputs, ... }:
+{ inputs, configLib, ... }:
 {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
@@ -24,4 +24,5 @@
       config.allowUnfree = true;
     };
   };
+  imports = (configLib.scanPaths ./.);
 }
