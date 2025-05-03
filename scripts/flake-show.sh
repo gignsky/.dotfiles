e1f81721575c73713dd2d37046dcd9978b6efe8d
gignsky <gig@gignsky.com>
2025-05-03 17:45:47 -0400

    removed flake-show script

--- scripts/flake-show.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# if no extra args just run 'nix flake show --all-systems' in the current directory, else if there is an argument to this script run 'nix flake show --all-systems $1'
-if [ $# -eq 0 ]; then
-  om show .
-else
-  om show $1
-fi
