c329aa52aadd17a337890e25693b7d6b703ee400
gignsky <gig@gignsky.com>
2025-05-22 23:21:29 -0400

    formatted vim.nix

--- home/gig/common/core/vim.nix
+++ home/gig/common/core/vim.nix
@@ -4,9 +4,4 @@
   home.packages = [
     inputs.neve.packages.${pkgs.system}.default
   ];
-  # # Debug statement to ensure the file is being processed
-  # environment.etc."gitconfig".text = ''
-  #     [pull]
-  #         rebase = true
-  # '';
 }
