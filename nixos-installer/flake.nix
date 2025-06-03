5c00e80c946c5835a5a6bb1294b4bee19f2be217
Maxwell Rupp <gig@gignsky.com>
2025-06-02 20:51:08 -0400

    commented out old boots

--- nixos-installer/flake.nix
+++ nixos-installer/flake.nix
@@ -50,11 +50,11 @@
         # grief = newConfig "grief" "/dev/vda" false "0";
         # guppy = newConfig "guppy" "/dev/vda" false "0";
         # gusto = newConfig "gusto" "/dev/sda" true "8";
-        merlin = newConfig "merlin" "/dev/nvme0n1" false;
+        # merlin = newConfig "merlin" "/dev/nvme0n1" false;
         # testbuzz1 = newConfig "testbuzz1" "/dev/sda" true;
         # buzz = newConfig "buzz" "/dev/sda" true;
         # cams-countertop = newConfig "cams-countertop" "/dev/sda" true;
-        tdarr-node = newConfig "tdarr-node" "/dev/sda" true;
+        # tdarr-node = newConfig "tdarr-node" "/dev/sda" true;
 
         # Custom ISO
         #
