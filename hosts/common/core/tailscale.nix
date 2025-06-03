0604b43d155844be2fbebe4fb226219f26bf8185
Maxwell Rupp <gig@gignsky.com>
2025-06-03 18:22:00 -0400

    installed tailscale

---
+++ hosts/common/core/tailscale.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  services.tailscale = {
+    enable = true;
+    # package = inputs.tailscale.packages.${pkgs.system}.default;
+    authKeyFile = /etc/tailscale/creds;
+    useRoutingFeatures = "both";
+    extraUpFlags = [
+      "--ssh"
+      "--accept-routes"
+      "--advertise-routes=172.29.118.0/20"
+    ];
+  };
+}
