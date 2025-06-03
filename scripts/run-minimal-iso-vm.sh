8d3c7d692bb6b27f175cfc8fd21a795a745e6213
Maxwell Rupp <gig@gignsky.com>
2025-06-02 20:46:22 -0400

    updated to work with iso

--- scripts/run-minimal-iso-vm.sh
+++ scripts/run-minimal-iso-vm.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+ISO_PATH="$1"
+DISK_IMG="$2"
+
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
-  -cdrom "${minimalIsoPath}" \
+  -cdrom "${ISO_PATH}" \
   -boot d \
-  -enable-kvm \
+#   -enable-kvm \
   -net nic -net user,hostfwd=tcp::2222-:22 \
-  -drive file=minimal-vm.img,format=qcow2,if=virtio
+  -drive file="${DISK_IMG}",format=qcow2,if=virtio
