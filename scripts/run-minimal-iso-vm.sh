64313f780ac792ef0f5848f5a686582913215c2d
Maxwell Rupp <gig@gignsky.com>
2025-06-03 05:26:41 -0400

    fixed up vm

--- scripts/run-minimal-iso-vm.sh
+++ scripts/run-minimal-iso-vm.sh
@@ -8,7 +8,5 @@ qemu-system-x86_64 \
   -cdrom "${ISO_PATH}" \
   -boot d \
   # -enable-kvm \
-  # -net nic -net user,hostfwd=tcp::2222-:22 \
-  -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no \
-  -device virtio-net-pci,netdev=mynet0 \
+  -net nic -net user,hostfwd=tcp::2222-:22 \
   -drive file="${DISK_IMG}",format=qcow2,if=virtio
