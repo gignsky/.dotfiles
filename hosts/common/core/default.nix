2e6b659c450671d4f3000a8dfe2b008d396b1f65
Maxwell Rupp <gig@gignsky.com>
2025-06-03 18:58:10 -0400

    formatting: ran nix fmt

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -1,17 +1,17 @@
 { inputs, outputs, configLib, ... }:
 
 {
-    imports = (configLib.scanPaths ./.);
-        # ++ [ inputs.home-manager.nixosModules.home-manager ] # TODO: IMPORT THIS when home-manager is integrated as a module
-        # ++ (builtins.attrValues outputs.nixosModules); # TODO: IMPORT THIS when implementing the modules subdirectory
+  imports = (configLib.scanPaths ./.);
+  # ++ [ inputs.home-manager.nixosModules.home-manager ] # TODO: IMPORT THIS when home-manager is integrated as a module
+  # ++ (builtins.attrValues outputs.nixosModules); # TODO: IMPORT THIS when implementing the modules subdirectory
 
-    nixpkgs = {
-        # you can add global overlays here
-        # overlays = builtins.attrValues outputs.overlays;
-        config = {
-        allowUnfree = true;
-        };
+  nixpkgs = {
+    # you can add global overlays here
+    # overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
     };
+  };
 
-    # hardware.enableRedistributableFirmware = true;
+  # hardware.enableRedistributableFirmware = true;
 }
