459869f92385b27e57e8b21cda25d8756598b41b
Maxwell Rupp <gig@gignsky.com>
2025-06-12 18:30:37 -0400

    changed boot order

--- scripts/run-iso-vm.sh
+++ scripts/run-iso-vm.sh
@@ -21,7 +21,7 @@ qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
   -cdrom "${ISO_PATH}" \
-  -boot d \
+  -boot order=dc \
   # -enable-kvm \
   -net nic -net user,hostfwd=tcp::2222-:22 \
   -drive file="${DISK_IMG}",format=qcow2,if=virtio \
