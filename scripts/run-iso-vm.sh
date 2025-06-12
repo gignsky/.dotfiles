c12d9d5fe1a1a773727459dfa65765d38dfb06a9
Maxwell Rupp <gig@gignsky.com>
2025-06-12 17:46:13 -0400

    added 15G hdd

--- scripts/run-iso-vm.sh
+++ scripts/run-iso-vm.sh
@@ -2,6 +2,14 @@
 ISO_PATH="$1"
 DISK_IMG="$2"
 
+EXTRA_DISK="/tmp/vm-extra.qcow2"
+qemu-img create -f qcow2 "$EXTRA_DISK" 15G
+
+cleanup() {
+  rm -f "$EXTRA_DISK"
+}
+trap cleanup EXIT
+
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
@@ -9,4 +17,5 @@ qemu-system-x86_64 \
   -boot d \
   # -enable-kvm \
   -net nic -net user,hostfwd=tcp::2222-:22 \
-  -drive file="${DISK_IMG}",format=qcow2,if=virtio
+  -drive file="${DISK_IMG}",format=qcow2,if=virtio \
+  -drive file="${EXTRA_DISK}",format=qcow2,if=virtio
