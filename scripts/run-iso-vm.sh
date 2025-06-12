9a67d29f31fbc191581a12a47a2cc6a1a6815a4c
Maxwell Rupp <gig@gignsky.com>
2025-06-12 18:00:45 -0400

    fixed 15G disk and added justfile logic to make it tmp

--- scripts/run-iso-vm.sh
+++ scripts/run-iso-vm.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 ISO_PATH="$1"
 DISK_IMG="$2"
+CREATE_EXTRA_DISK="$3"
+CLEANUP_EXTRA_DISK="$4"
 
 EXTRA_DISK="/tmp/vm-extra.qcow2"
-qemu-img create -f qcow2 "$EXTRA_DISK" 15G
 
-cleanup() {
-  rm -f "$EXTRA_DISK"
-}
-trap cleanup EXIT
+if [[ "$CREATE_EXTRA_DISK" == "yes" && ! -f "$EXTRA_DISK" ]]; then
+  qemu-img create -f qcow2 "$EXTRA_DISK" 15G
+fi
+
+if [[ "$CLEANUP_EXTRA_DISK" == "yes" ]]; then
+  cleanup() {
+    rm -f "$EXTRA_DISK"
+  }
+  trap cleanup EXIT
+fi
+
+EXTRA_DRIVE_ARG="-drive file=${EXTRA_DISK},format=qcow2,if=virtio"
 
 qemu-system-x86_64 \
   -m 2048 \
@@ -18,4 +27,4 @@ qemu-system-x86_64 \
   # -enable-kvm \
   -net nic -net user,hostfwd=tcp::2222-:22 \
   -drive file="${DISK_IMG}",format=qcow2,if=virtio \
-  -drive file="${EXTRA_DISK}",format=qcow2,if=virtio
+  $EXTRA_DRIVE_ARG
