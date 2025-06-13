f96676eab80a61efa2731edb048ce1db3d94daed
Maxwell Rupp <gig@gignsky.com>
2025-06-12 20:10:24 -0400

    fixed vm disk problems

--- scripts/run-iso-vm.sh
+++ scripts/run-iso-vm.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 ISO_PATH="$1"
 DISK_IMG="$2"
-CLEANUP_EXTRA_DISK="$3"
-
-EXTRA_DISK="/tmp/vm-extra.qcow2"
-
-# create the extra disk
-qemu-img create -f raw "$EXTRA_DISK" 15G
-
-if [[ "$CLEANUP_EXTRA_DISK" == "yes" ]]; then
-  cleanup() {
-    rm -f "$EXTRA_DISK"
-  }
-  trap cleanup EXIT
-fi
 
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
   -cdrom "${ISO_PATH}" \
+  -d guest_errors \
   -boot order=dc \
-  # -enable-kvm \
   -net nic -net user,hostfwd=tcp::2222-:22 \
   -drive file="${DISK_IMG}",format=qcow2,if=virtio \
-  -drive file=${EXTRA_DISK},format=qcow2,if=ide
+  # -enable-kvm \
