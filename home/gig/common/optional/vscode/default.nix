a5a722d177ac429f4fa59e71215e528f03c156ba
Maxwell Rupp <gig@gignsky.com>
2025-06-13 00:57:45 -0400

    got vscode building - not installing

--- home/gig/common/optional/vscode/default.nix
+++ home/gig/common/optional/vscode/default.nix
@@ -1,47 +1,39 @@
-{ inputs, outputs, configLib, pkgs, lib, ... }:
+{ config, lib, pkgs, isWSL ? false, ... }:
 
 let
   extensionIds = import ../vscode-extensions-list.nix;
   extensions = builtins.map (id: pkgs.vscode-extensions.${id}) extensionIds;
+  scriptPath = "${config.home.homeDirectory}/.dotfiles/scripts/install-vscode-extensions-wsl.sh";
 in
-{
-  options.isWSL = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Set to true if running in WSL.";
-  };
+lib.mkMerge [
+  (lib.mkIf isWSL {
+    home.activation.vscodeExtensionsWSL = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      chmod +x ${scriptPath} || true
+      ${scriptPath} || true
+    '';
+  })
+  (lib.mkIf (!isWSL) {
+    # info at
+    # https://nixos.wiki/wiki/Visual_Studio_Code
+    ##############################################
 
-  config = config:
-    if config.isWSL then {
-      home.activation.vscodeExtensionsWSL = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        ${config.home.homeDirectory}/.dotfiles/scripts/install-vscode-extensions-wsl.sh || true
-      '';
-    } else {
-      # info at
-      # https://nixos.wiki/wiki/Visual_Studio_Code
-      ##############################################
+    # search extentions at
+    # https://search.nixos.org/packages?type=packages&query=vscode-extensions
+    ##############################################
 
-      # search extentions at
-      # https://search.nixos.org/packages?type=packages&query=vscode-extensions
-      ##############################################
-
-      # Home Manager Way
-      programs = {
-        vscode = {
-          enable = false;
-          enableExtensionUpdateCheck = true;
-          enableUpdateCheck = true;
-          mutableExtensionsDir = false; # disallows vscode from installing its own extensions
-          # copy and paste user settings into the field below
-          userSettings = { };
-          extensions = extensions;
-        };
-
-        # # A "wonderful CLI to track your time"
-        # watson = {
-        #     enable = false;
-        #     enableZshIntegration = true;
-        # };
-      };
+    # Home Manager Way
+    programs.vscode = {
+      enable = false;
+      enableExtensionUpdateCheck = true;
+      enableUpdateCheck = true;
+      mutableExtensionsDir = false; # disallows vscode from installing its own extensions
+      userSettings = { };
+      extensions = extensions;
     };
-}
+    # # A "wonderful CLI to track your time"
+    # watson = {
+    #     enable = false;
+    #     enableZshIntegration = true;
+    # };
+  })
+]
--- home/gig/wsl.nix
+++ home/gig/wsl.nix
@@ -1,22 +1,21 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{
-  # inputs,
-  outputs
-  # , config,
+{ inputs
+, outputs
 , pkgs
+, lib
+, configLib
+, config
 , ...
 }:
 {
-  # You can import other home-manager modules here
   imports = [
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # ./common/core
     ./home.nix
+    ./common/optional/vscode
     # ./common/optional/vim.nix
   ];
+
+  _module.args.isWSL = true;
 
   nixpkgs = {
     # You can add overlays here
@@ -36,7 +35,4 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.05";
-
-  # Set for WSL-specific logic
-  isWSL = true;
 }
