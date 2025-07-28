988233386ba64e9f6539995e651eb747131e8666
Maxwell Rupp <gig@gignsky.com>
2025-07-28 00:28:23 -0400

    added a bunch of options from the isMaximal nvf config

--- home/gig/common/core/vim.nix
+++ home/gig/common/core/vim.nix
@@ -11,18 +11,56 @@
         viAlias = true;
         vimAlias = true;
         lsp = {
+          # This must be enabled for the language modules to hook into
+          # the LSP API.
           enable = true;
+
+          formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = false;
         };
         languages = {
           enableFormat = true;
           enableExtraDiagnostics = true;
-          enableTreesitter = false;
+          enableTreesitter = true;
           nix.enable = true;
-          rust.enable = true;
+          rust = {
+            enable = true;
+            crates.enable = true;
+          };
           python.enable = false;
           markdown.enable = true;
           html.enable = true;
+          css.enable = false;
           sql.enable = true;
+          bash.enable = true;
+          svelte.enable = false;
+        };
+        visuals = {
+          nvim-scrollbar.enable = true;
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          cinnamon-nvim.enable = false;
+          fidget-nvim.enable = false;
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+
+          # noted as fun :)
+          cellular-automaton.enable = false;
+        };
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "catppuccin";
+          };
+        };
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+          transparent = false;
         };
         clipboard.enable = true;
         globals = {
