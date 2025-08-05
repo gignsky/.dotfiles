c4fd9c8eeda98f4061ccbfb392b93fd2b60b786f
Maxwell Rupp <gig@gignsky.com>
2025-08-04 21:14:13 -0400

    turned on starship and zoxide

--- home/gig/common/optional/nushell.nix
+++ home/gig/common/optional/nushell.nix
@@ -1,7 +1,26 @@
-_: {
+{ pkgs, ... }: {
   programs.nushell = {
     enable = true;
     # package = "${pkgs.nushell}/bin/nu";
     shellAliases = import ./shellAliases.nix;
+    settings = {
+      show_banner = false;
+      completions.external = {
+        enable = true;
+        max_results = 200;
+      };
+      buffer_editor = "vi";
+    };
+    plugins = with pkgs.nushellPlugins; [
+      net
+      highlight
+      units
+      formats
+      query
+    ];
+  };
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
   };
 }
