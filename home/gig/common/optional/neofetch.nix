0b03f56ae445999b1a0bf91cf5750b9fd0bbbbce
Maxwell Rupp <gig@gignsky.com>
2025-08-07 09:05:26 -0400

    gig@wsl: 2025-08-07 09:03 : id 178 -> /nix/store/n6w2if2bmdj8v6sd4anqq4fibi1dddj1-home-manager-generation

--- home/gig/common/optional/neofetch.nix
+++ home/gig/common/optional/neofetch.nix
@@ -1,15 +1,18 @@
 { inputs, ... }:
 {
-  imports = [
-    inputs.nufetch.homeManagerModules.nufetch
-  ];
+  imports = [ inputs.nufetch.homeManagerModules.nufetch ];
 
   programs.nufetch = {
     enable = true;
+    packages = true;
+    resolution = true;
+    theme = true;
+    icons = true;
+    terminal_font = true;
+    cpu_usage = true;
     public_ip = true;
-    # local_ip = false;
-    # extraPrintInfoFields = ''
-    #   info "Public IP" public_ip
-    # '';
+    local_ip = true;
+    song = true;
+    users = true;
   };
 }
