e02f49063a88ae56c8f809d1a5df5bb5c72b3fc6
Maxwell Rupp <gig@gignsky.com>
2025-08-06 20:43:44 -0400

    added script

---
+++ scripts/home-manager-flake-rebuild.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -n "$1" ]; then
+	export HOST="$1"
+else
+	if [ "$(hostname)" = "nixos" ]; then
+		HOST="wsl"
+	else
+		HOST="$(hostname)"
+	fi
+	export HOST
+fi
+
+set -e
+pushd ~/.dotfiles || exit
+git diff -U0 ./*glob*.nix
+echo "NixOS Rebuilding..."
+sudo home-manager switch --flake .#"gig@$HOST" | sudo tee home-manager-switch.log || (
+ grep --color error && false) < home-manager-switch.log
+gen=$(home-manager generations 2>/dev/null | head -n 1)
+git commit -am "gig@$HOST: $gen"
+popd || exit
