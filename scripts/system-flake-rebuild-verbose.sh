b67a0941f25bcd8a8c429724ae3e9faff5fd4833
Maxwell Rupp <gig@gignsky.com>
2025-08-12 22:19:53 -0400

    updated scripts to run pre-commit first

--- scripts/system-flake-rebuild-verbose.sh
+++ scripts/system-flake-rebuild-verbose.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 if [ -n "$1" ]; then
-	export HOST="$1"
+  export HOST="$1"
 else
-	if [ "$(hostname)" = "nixos" ]; then
-		HOST="wsl"
-	else
-		HOST="$(hostname)"
-	fi
-	export HOST
+  if [ "$(hostname)" = "nixos" ]; then
+    HOST="wsl"
+  else
+    HOST="$(hostname)"
+  fi
+  export HOST
 fi
 
 set -e
-pushd ~/.dotfiles || exit
+pushd . || exit
 git diff -U0 ./*glob*.nix
+echo "Running pre-commit on all files"
+nix develop -c pre-commit run --all-files
 echo "NixOS Rebuilding..."
 sudo nixos-rebuild switch -vv --flake .#"$HOST" | sudo tee nixos-switch.log || (
- grep --color error && false) < nixos-switch.log
+  grep --color error && false
+) <nixos-switch.log
 gen=$(nixos-rebuild list-generations 2>/dev/null | grep current)
 git commit -am "$HOST: $gen"
 popd || exit
