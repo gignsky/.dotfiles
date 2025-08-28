0fb80434e8bb889e56439342320b6815825d6810
Maxwell Rupp <maxwell@giglab.dev>
2025-08-28 14:26:04 -0400

    testing removing ssh config

--- home/gig/common/core/ssh.nix
+++ home/gig/common/core/ssh.nix
@@ -8,16 +8,16 @@
         identitiesOnly = true;
         identityFile = "/home/gig/.ssh/id_rsa";
       };
-      "spacedock" = {
-        hostname = "192.168.51.2";
-        identitiesOnly = true;
-        identityFile = "/home/gig/.ssh/gment";
-      };
-      "giglab" = {
-        hostname = "giglab.dev";
-        identitiesOnly = true;
-        identityFile = "/home/gig/.ssh/gment";
-      };
+      # "spacedock" = {
+      #   hostname = "192.168.51.2";
+      #   identitiesOnly = true;
+      #   identityFile = "/home/gig/.ssh/gment";
+      # };
+      # "giglab" = {
+      #   hostname = "giglab.dev";
+      #   identitiesOnly = true;
+      #   identityFile = "/home/gig/.ssh/gment";
+      # };
     };
   };
 }
