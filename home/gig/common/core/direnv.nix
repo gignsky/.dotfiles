49105051519976c828f207dd88c4987789fbcb22
Maxwell Rupp <gig@gignsky.com>
2025-09-17 18:07:24 -0400

    Revert "updated path to direnv.toml"
    This reverts commit 27bf1ece2aa2560914a7262435422c197f3ed973.

--- home/gig/common/core/direnv.nix
+++ home/gig/common/core/direnv.nix
@@ -1,10 +1,11 @@
-{ lib, configLib, ... }:
+{ configLib, ... }:
 {
   programs.direnv = {
     enable = true;
     enableNushellIntegration = false; # Disable automatic integration due to syntax issues
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    configToml = lib.readFile configLib.relativeToRoot "home/gig/common/resources/direnv.toml";
   };
+
+  home.file.".config/direnv/direnv.toml".source = configLib.relativeToRoot "home/gig/common/resources/direnv.toml";
 }
