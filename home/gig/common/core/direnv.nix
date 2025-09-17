dc33335441f9dfbfbed5891cadda69d360bdc018
Maxwell Rupp <gig@gignsky.com>
2025-09-17 18:06:10 -0400

    updated direnv.toml path

--- home/gig/common/core/direnv.nix
+++ home/gig/common/core/direnv.nix
@@ -1,10 +1,14 @@
 { lib, configLib, ... }:
+let
+  direnvTomlPath = configLib.relativeToRoot "home/gig/common/resources/direnv.toml";
+in
+
 {
   programs.direnv = {
     enable = true;
     enableNushellIntegration = false; # Disable automatic integration due to syntax issues
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    configToml = lib.readFile configLib.relativeToRoot "home/gig/common/resources/direnv.toml";
+    configToml = lib.readFile direnvTomlPath;
   };
 }
