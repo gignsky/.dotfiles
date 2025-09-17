b135cbed8115991c6bc943470d01053079d61bc1
Maxwell Rupp <maxwell@giglab.dev>
2025-09-17 14:44:41 -0400

    added additional nmap alias

--- home/gig/common/optional/shellAliases.nix
+++ home/gig/common/optional/shellAliases.nix
@@ -32,6 +32,7 @@
   # nmap
   localnmap = "nix shell nixpkgs#lolcat -c nix shell nixpkgs#nmap -c nmap -v -sn --open 192.168.51.0/24 | lolcat";
   spacenmap = "nix shell nixpkgs#lolcat -c nix shell nixpkgs#nmap -c nmap -A -T4 192.168.51.2 | lolcat";
+  fullnmap = "nix shell nixpkgs#lolcat -c nix shell nixpkgs#nmap -c nmap -A -T4 -v --open 192.168.51.0/24 | lolcat";
 
   # Git
   lg = "lazygit";
