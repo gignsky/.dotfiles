fbd9c40e66334f06eaf12fa437d3b8d012685e61
nixos <nixos@gignsky.com>
2025-10-27 21:56:53 -0400

    added ganosLal home file

---
+++ home/gig/ganosLal.nix
@@ -0,0 +1,30 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  # inputs,
+  pkgs,
+  ...
+}:
+{
+  # You can import other home-manager modules here
+  imports = [
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    ./home.nix
+    # ./common/optional/vscode
+    # ./cams-countertop.nix
+  ];
+
+  home.packages = with pkgs; [
+    # plex-media-player
+    # remmina
+    # bitwarden-cli
+    # bitwarden-desktop
+    # discord
+    # anydesk
+  ];
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "25.05";
+}
