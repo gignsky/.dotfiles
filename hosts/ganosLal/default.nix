52af5c215dea797e212d2e68b35e6c4de73b731c
Maxwell Rupp <maxwell@giglab.dev>
2025-10-27 23:29:46 -0400

    switched gui to xfce.nix

--- hosts/ganosLal/default.nix
+++ hosts/ganosLal/default.nix
@@ -23,7 +23,8 @@
     (configLib.relativeToRoot "hosts/common/core")
 
     # optional
-    (configLib.relativeToRoot "hosts/common/optional/gui.nix")
+    (configLib.relativeToRoot "hosts/common/optional/xfce.nix")
+    # (configLib.relativeToRoot "hosts/common/optional/wayland.nix")
     # (configLib.relativeToRoot "hosts/common/optional/firefox.nix")
     # ../common/optional/xrdp.nix
 
