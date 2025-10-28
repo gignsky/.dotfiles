19a0871a9c7499464e1fc8f64bb81c56f3f93390
Maxwell Rupp <maxwell@giglab.dev>
2025-10-28 06:17:22 -0400

    removed hyprland settings so default will take over

--- home/gig/common/optional/hyprland/default.nix
+++ home/gig/common/optional/hyprland/default.nix
@@ -17,28 +17,28 @@
     wayland.windowManager.hyprland = {
       enable = true;
 
-      settings = {
-        general = {
-          gaps_in = 5;
-          gaps_out = 10;
-          border_size = 2;
-          "col.active_border" = "rgba(94e2d5ee) rgba(92e2d5ee) 45deg";
-        };
-        exec-once = [
-          "hyprpaper"
-          "waybar"
-        ];
-        decoration = {
-          rounding = 8;
-        };
-        gestures = {
-          workspace_swipe = true;
-        };
-        env = [
-          "NIXOS_OZONE_WL,1"
-          "MOZ_ENABLE_WAYLAND,1"
-        ];
-      };
+      # settings = {
+      #   general = {
+      #     gaps_in = 5;
+      #     gaps_out = 10;
+      #     border_size = 2;
+      #     "col.active_border" = "rgba(94e2d5ee) rgba(92e2d5ee) 45deg";
+      #   };
+      #   exec-once = [
+      #     "hyprpaper"
+      #     "waybar"
+      #   ];
+      #   decoration = {
+      #     rounding = 8;
+      #   };
+      #   gestures = {
+      #     workspace_swipe = true;
+      #   };
+      #   env = [
+      #     "NIXOS_OZONE_WL,1"
+      #     "MOZ_ENABLE_WAYLAND,1"
+      #   ];
+      # };
     };
   };
 
