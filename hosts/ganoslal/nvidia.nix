705ec7b7f2f07ff6768011417b2fa1294f4172a2
Maxwell Rupp <maxwell@giglab.dev>
2025-10-28 02:30:56 -0400

    added nvidia settings

--- hosts/ganoslal/nvidia.nix
+++ hosts/ganoslal/nvidia.nix
@@ -1,3 +1,19 @@
+{ config, lib, ... }:
+# Ref. https://nixos.wiki/wiki/Nvidia
 {
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  nixpkgs.config.allowUnfree = lib.mkForce true;
+  hardware = {
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      prime = {
+        sync.enable = true;
+        nvidiaBusId = "PCI:45:0:0";
+        amdgpuBusId = "PCI:23:0:0";
+      };
+    };
+    graphics.enable = true;
+  };
 }
