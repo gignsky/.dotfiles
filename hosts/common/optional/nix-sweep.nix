8074c111ac53d17584b2efa09a44595a51418655
Maxwell Rupp <maxwell@giglab.dev>
2025-11-12 04:20:28 -0500

    prepped nix-sweep.nix for local nixpkgs version

--- hosts/common/optional/nix-sweep.nix
+++ hosts/common/optional/nix-sweep.nix
@@ -1,9 +1,18 @@
-{ inputs, pkgs, ... }:
+{ outputs, pkgs, ... }:
 let
   inherit (pkgs.stdenv.hostPlatform) system;
 in
 {
-  environment.systemPackages = with inputs; [
-    nix-sweep.packages.${system}.default
-  ];
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.local-packages
+      outputs.overlays.unstable-packages
+    ];
+  };
+  # environment.systemPackages = with inputs; [
+  #   nix-sweep.packages.${system}.default
+  # ];
 }
