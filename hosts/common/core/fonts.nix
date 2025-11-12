810f6a6688a3774caf04269cc3fa6b8add49be40
Maxwell Rupp <maxwell@giglab.dev>
2025-11-11 23:08:43 -0500

    updated font order

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -3,13 +3,35 @@ let
   inherit (pkgs.stdenv.hostPlatform) system;
 in
 {
-  fonts.packages =
-    with inputs;
-    with pkgs;
-    [
-      fancy-fonts.packages.${system}.cartograph
-      fancy-fonts.packages.${system}.artifex
-      fancy-fonts.packages.${system}.monolisa
-      nerd-fonts.go-mono
-    ];
+  fonts = {
+    packages =
+      with inputs;
+      with pkgs;
+      [
+        fancy-fonts.packages.${system}.cartograph
+        fancy-fonts.packages.${system}.artifex
+        fancy-fonts.packages.${system}.monolisa
+        nerd-fonts.go-mono
+      ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [
+          "Cartograph CF"
+          "Monolisa Variable"
+          "GoMono Nerd Font Mono"
+        ];
+        sansSerif = [
+          "Monolisa Variable"
+        ];
+        serif = [
+          "Artifex CF"
+        ];
+        # Set emoji font if needed
+        # emoji = [
+        #   "TEST"
+        # ]
+      };
+    };
+  };
 }
