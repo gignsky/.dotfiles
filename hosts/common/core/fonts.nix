b091756656df874c4b5bd3a528a8a4efb1cd6b03
Maxwell Rupp <maxwell@giglab.dev>
2025-11-11 22:17:00 -0500

    wsl: 143 current  2025-11-11 22:16:47  25.05.20251010.7e297dd  Unknown                          *

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -1,4 +1,7 @@
-{ inputs, system, ... }:
+{ inputs, pkgs, ... }:
+let
+  inherit (pkgs.stdenv.hostPlatform) system;
+in
 {
   environment.systemPackages = with inputs; [
     fancy-fonts.packages.${system}.cartograph
