d1c896a4efe25423ade3c770ed054b622f662a59
Maxwell Rupp <maxwell@giglab.dev>
2025-11-11 22:54:30 -0500

    wsl: 147 current  2025-11-11 22:54:23  25.05.20251010.7e297dd  Unknown                          *

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -3,14 +3,13 @@ let
   inherit (pkgs.stdenv.hostPlatform) system;
 in
 {
-  environment.systemPackages = with inputs; [
-    fancy-fonts.packages.${system}.cartograph
-    fancy-fonts.packages.${system}.artifex
-    fancy-fonts.packages.${system}.monolisa
-  ];
-
-  # fav nerd font
-  fonts.packages = with pkgs; [
-    nerd-fonts.go-mono
-  ];
+  fonts.packages =
+    with inputs;
+    with pkgs;
+    [
+      fancy-fonts.packages.${system}.cartograph
+      fancy-fonts.packages.${system}.artifex
+      fancy-fonts.packages.${system}.monolisa
+      nerd-fonts.go-mono
+    ];
 }
