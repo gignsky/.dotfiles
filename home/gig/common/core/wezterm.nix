991fa823aa52b197feb4a5cb7eb64e0b800d9428
Maxwell Rupp <maxwell@giglab.dev>
2025-11-20 22:17:55 -0500

    configured wezterm!

---
+++ home/gig/common/core/wezterm.nix
@@ -0,0 +1,33 @@
+{
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+      local wezterm = require("wezterm")
+
+      local config = wezterm.config_builder()
+
+      config.font_size = 14.0
+
+      config.hide_tab_bar_if_only_one_tab = true
+
+      config.window_padding = {
+      	left = 0,
+      	right = 0,
+      	top = 0,
+      	bottom = 0,
+      }
+
+      config.window_decorations = "RESIZE"
+
+      -- config.keys = {
+      --	{
+      --		key = "Enter",
+      --		mods = "ALT",
+      --		action = wezterm.action.DisableDefaultAssignment,
+      --	},
+      -- }
+
+      return config
+    '';
+  };
+}
