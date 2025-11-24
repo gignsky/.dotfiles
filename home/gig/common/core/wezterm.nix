415cf8e3b25a124ff4e56bbb62b2023865d1f2a0
Maxwell Rupp <maxwell@giglab.dev>
2025-11-24 17:30:12 -0500

    updated font

--- home/gig/common/core/wezterm.nix
+++ home/gig/common/core/wezterm.nix
@@ -7,6 +7,7 @@
       local config = wezterm.config_builder()
 
       config.font_size = 14.0
+      config.font = wezterm.font("MonoLisa Variable")
 
       config.hide_tab_bar_if_only_one_tab = true
 
