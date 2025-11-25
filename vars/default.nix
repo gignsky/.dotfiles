7e08534cb8b1102ea78a99ba4bf174a9f877df06
Maxwell Rupp <maxwell@giglab.dev>
2025-11-25 18:51:22 -0500

    removed dupe line

--- vars/default.nix
+++ vars/default.nix
@@ -8,8 +8,6 @@
   #domain = inputs.nix-secrets.domain;
   #userFullName = inputs.nix-secrets.full-name;
   handle = "gignsky";
-  uid = 1701;
-  guid = 1701;
   #userEmail = inputs.nix-secrets.user-email;
   # gitHubEmail = "7410928+emergentmind@users.noreply.github.com";
   # gitLabEmail = "2889621-emergentmind@users.noreply.gitlab.com";
