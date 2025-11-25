a72db0debe4e335342bac861a8fa3f60f46c26d0
Maxwell Rupp <maxwell@giglab.dev>
2025-11-25 15:56:41 -0500

    added tailscale back

--- hosts/merlin/default.nix
+++ hosts/merlin/default.nix
@@ -26,6 +26,7 @@
     # (configLib.relativeToRoot "hosts/common/optional/xfce.nix")
     (configLib.relativeToRoot "hosts/common/optional/bspwm.nix") # Enable bspwm window manager
     (configLib.relativeToRoot "hosts/common/optional/firefox.nix")
+    (configLib.relativeToRoot "hosts/common/optional/tailscale.nix")
     # ../common/optional/xrdp.nix
 
     #gig users
