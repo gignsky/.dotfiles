46a931da9221b04b7a0a9af3b594e529ff0979fe
Maxwell Rupp <maxwell@giglab.dev>
2025-11-24 22:37:08 -0500

    attempting bspwm attempt #2

--- hosts/merlin/default.nix
+++ hosts/merlin/default.nix
@@ -24,6 +24,7 @@
 
     # optional
     (configLib.relativeToRoot "hosts/common/optional/xfce.nix")
+    (configLib.relativeToRoot "hosts/common/optional/bspwm.nix") # Enable bspwm window manager
     (configLib.relativeToRoot "hosts/common/optional/firefox.nix")
     # ../common/optional/xrdp.nix
 
