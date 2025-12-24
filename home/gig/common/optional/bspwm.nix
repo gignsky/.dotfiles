b5e5776b11f12d744b488244d61c69fcfd8e0c1a
Maxwell Rupp <maxwell@giglab.dev>
2025-12-23 20:45:42 -0500

    gig@merlin: 2025-12-23 20:45 : id 94 -> /nix/store/9ln1j9b0r96z2a31vhkjqdnkak9pyfkk-home-manager-generation (current)

--- home/gig/common/optional/bspwm.nix
+++ home/gig/common/optional/bspwm.nix
@@ -66,9 +66,9 @@
         module-margin-right = 1;
 
         # Font configuration
-        font-0 = "DejaVu Sans:size=10;2";
-        font-1 = "Font Awesome 6 Free:style=Solid:size=10;2";
-        font-2 = "Font Awesome 6 Brands:size=10;2";
+        font-0 = "Cartograph CF:size=10;2";
+        font-1 = "Monolisa Variable:size=10;2";
+        font-2 = "GoMono Nerd Font Mono:size=10;2";
 
         # Module layout
         modules-left = "bspwm";
