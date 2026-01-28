e43657973935a7618e0056fa7cd98affd68c4d87
Maxwell Rupp <maxwell@giglab.dev>
2026-01-28 06:20:31 -0500

    added nps

---
+++ hosts/common/core/nps.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.nps
+  ];
+
+  # forcing flake mode
+  environment.variables = {
+    NIX_PACKAGE_SEARCH_EXPERIMENTAL = "true";
+    # NIX_PACKAGE_SEARCH_MULTI_LINE = "true";
+    NIX_PACKAGE_SEARCH_TRUNCATE = "true"; # Can only be enabled if multi-line is off
+  };
+
+  # automating cache refresh
+  systemd.timers."refresh-nps-cache" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      # DEFAULT: # OnCalendar = "weekly"; # or however often you'd like
+      OnCalendar = "hourly"; # or however often you'd like
+      Persistent = true;
+      Unit = "refresh-nps-cache.service";
+    };
+  };
+
+  systemd.services."refresh-nps-cache" = {
+    # Make sure `nix` and `nix-env` are findable by systemd.services.
+    path = [ "/run/current-system/sw/" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "REPLACE_ME"; # ⚠️ replace with your "username" or "${user}", if it's defined
+    };
+    script = ''
+      set -eu
+      echo "Start refreshing nps cache..."
+      # ⚠️ note the use of overlay (as described above), adjust if needed
+      # ⚠️ use `nps -dddd -e -r` if you use flakes
+      ${pkgs.nps}/bin/nps -r -dddd
+      echo "... finished nps cache with exit code $?."
+    '';
+  };
+}
