921f699a8b6481c38caeb2badb96bf46f4484af8
Maxwell Rupp <maxwell@giglab.dev>
2026-01-28 14:44:24 -0500

    turn polybar back off

--- home/gig/common/optional/bspwm.nix
+++ home/gig/common/optional/bspwm.nix
@@ -41,7 +41,7 @@
 
   # Polybar status bar configuration
   services.polybar = {
-    enable = true;
+    enable = false;
     # package = pkgs.polybar.override {
     #   alsaSupport = true;
     #   githubSupport = true;
