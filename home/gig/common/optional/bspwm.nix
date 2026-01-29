198ed6512d7cd40a84b11e305c3451125717d452
Maxwell Rupp <maxwell@giglab.dev>
2026-01-28 23:00:24 -0500

    updated background

--- home/gig/common/optional/bspwm.nix
+++ home/gig/common/optional/bspwm.nix
@@ -33,20 +33,20 @@
       executable = true;
     };
     #TODO SCOTTY! REMIND ME to figure out how to make these roatate through the tolkien folder
-    # SGA wallpaper
-    ".background-image" = {
-      source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/SGA/Stargate_Atlantis_Gate_Fixed_Centered_2560x1440.png";
-    };
+    # # SGA wallpaper
+    # ".background-image" = {
+    #   source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/SGA/Stargate_Atlantis_Gate_Fixed_Centered_2560x1440.png";
+    # };
     # ".background-image" = {
     #   source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/SGA/Stargate_Atlantis_Gate_Fixed_Centered_5K.png";
     # };
     # ".background-image" = {
     #   source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/SGA/Stargate_Atlantis_Gate_Fixed_Centered_5K Sharper.png";
     # };
-    # # LOTR wallpaper
-    # ".background-image" = {
-    #   source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/tolkien/desktop/4k-doors-of-durin-horizontal.webp";
-    # };
+    # LOTR wallpaper
+    ".background-image" = {
+      source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/tolkien/desktop/4k-doors-of-durin-horizontal.webp";
+    };
     # # NixOS logo wallpaper
     # ".background-image" = {
     #   source = configLib.relativeToRoot "home/gig/common/resources/wallpapers/nixos-logo.png";
