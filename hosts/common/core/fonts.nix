50058328271309e4905b79f524531e9aa2fc6ad1
Maxwell Rupp <maxwell@giglab.dev>
2026-02-27 17:00:00 -0500

    limiting fonts

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -10,28 +10,28 @@ in
       with inputs;
       with pkgs;
       [
-        fancy-fonts.packages.${system}.cartograph
-        fancy-fonts.packages.${system}.artifex
-        fancy-fonts.packages.${system}.monolisa
+        # fancy-fonts.packages.${system}.cartograph
+        # fancy-fonts.packages.${system}.artifex
+        # fancy-fonts.packages.${system}.monolisa
         nerd-fonts.go-mono
-        times-newer-roman
+        # times-newer-roman
       ];
     fontconfig = {
       enable = true;
       defaultFonts = {
         monospace = [
-          "MonoLisa Variable"
+          # "MonoLisa Variable"
           "GoMono Nerd Font Mono"
-          "Cartograph CF"
-          "Artifex CF"
-          "Times Newer Roman"
+          # "Cartograph CF"
+          # "Artifex CF"
+          # "Times Newer Roman"
         ];
-        sansSerif = [
-          "MonoLisa Variable"
-        ];
+        # sansSerif = [
+        #   "MonoLisa Variable"
+        # ];
         serif = [
-          "Artifex CF"
-          "Times Newer Roman"
+          # "Artifex CF"
+          # "Times Newer Roman"
           "GoMono Nerd Font Mono"
         ];
         # Set emoji font if needed
