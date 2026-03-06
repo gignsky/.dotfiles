c82c5fcd3a2b11eb311b615c32837ddbe8782637
Maxwell Rupp <maxwell@giglab.dev>
2026-03-06 18:58:07 -0500

    gig@merlin: 2026-03-06 18:58 : id 156 -> /nix/store/4zcfr223qkx6pphh27dv7b3y1mm5i0vi-home-manager-generation (current)

--- home/gig/merlin.nix
+++ home/gig/merlin.nix
@@ -19,11 +19,10 @@
 
   home.packages = with pkgs; [
     plex-desktop
-    # remmina
+    remmina
+    youtube-music
     # bitwarden-cli
     # bitwarden-desktop
-    ytmdesktop
-    # ytmdl # YT Music downloader
     discord
     makemkv
     # anydesk
