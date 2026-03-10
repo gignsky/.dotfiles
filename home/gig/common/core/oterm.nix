12878180ccfbc591790376b7f68fdb8fb51eff4d
Maxwell Rupp <maxwell@giglab.dev>
2026-03-10 03:55:08 -0400

    gig@wsl@ganoslal: 2026-03-10 03:55 : id 289 -> /nix/store/cfxcvm15xpjca9d43g1k1n3qiiqpd6rr-home-manager-generation (current)

--- home/gig/common/core/oterm.nix
+++ home/gig/common/core/oterm.nix
@@ -9,4 +9,11 @@
       OLLAMA_HOST = "http://192.168.51.3:30068";
     };
   };
+  programs.nushell = {
+    # This appends the variables directly to your env.nu file
+    envFile.text = ''
+      $env.OLLAMA_URL = "http://192.168.51.3:30068"
+      $env.OLLAMA_HOST = "http://192.168.51.3:30068"
+    '';
+  };
 }
