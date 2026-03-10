cee91bce4e0dc48fc4779bef7c9732ed88224753
Maxwell Rupp <maxwell@giglab.dev>
2026-03-10 02:02:51 -0400

    added home-manager tests back

--- checks/default.nix
+++ checks/default.nix
@@ -7,7 +7,7 @@ let
   nixosTests = import ./nixos-tests.nix { inherit lib self; };
   packageTests = import ./package-tests.nix { inherit lib pkgs; };
   openCodeTests = import ./opencode-tests.nix { inherit lib self pkgs; };
-  # homeManagerTests = import ./home-manager-tests.nix { inherit lib self; };
+  homeManagerTests = import ./home-manager-tests.nix { inherit lib self; };
 in
 # Merge all test attributes into a single flat set
-nixosTests // packageTests // openCodeTests # // homeManagerTests
+nixosTests // packageTests // openCodeTests // homeManagerTests
