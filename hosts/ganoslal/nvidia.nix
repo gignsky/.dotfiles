b6897344d1905f9bea7d381151691844ead08f16
Maxwell Rupp <maxwell@giglab.dev>
2026-03-10 20:09:55 -0400

    nvidia settings - set prop kernel drivers

--- hosts/ganoslal/nvidia.nix
+++ hosts/ganoslal/nvidia.nix
@@ -16,7 +16,7 @@
 
       # Use proprietary kernel modules (GTX 970 doesn't support open modules)
       # RTX 3060 Ti could use open=true with driver 560+, but GTX 970 requires proprietary
-      open = false;
+      open = true;
 
       # Enable NVIDIA Control Panel
       nvidiaSettings = true;
