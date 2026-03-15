0f4a288aff31fb433d8b04ba9cdc2deb766aeb1b
Maxwell Rupp <maxwell@giglab.dev>
2026-03-15 15:04:46 -0400

    added saved config to merlin's grub

--- hosts/merlin/default.nix
+++ hosts/merlin/default.nix
@@ -63,6 +63,12 @@
       efiInstallAsRemovable = false;
       useOSProber = true; # Automatically detect Windows and other OSes
       configurationLimit = 20; # Limit boot menu entries to last 20 generations
+
+      # default config
+      default = "saved";
+      extraConfig = ''
+        GRUB_SAVEDEFAULT=true
+      '';
     };
     efi.canTouchEfiVariables = true;
   };
