b17c578a9ed273d99cc0663cf1923c86e3086879
Maxwell Rupp <maxwell@giglab.dev>
2026-03-16 18:20:19 -0400

    added ganoslal autorandr config to home

--- home/gig/ganoslal.nix
+++ home/gig/ganoslal.nix
@@ -15,20 +15,44 @@
     ./common/optional/bspwm.nix # Enable bspwm window manager configuration
     # ./cams-countertop.nix
   ];
+  home = {
 
-  home.packages = with pkgs; [
-    # ytmdesktop
-    youtube-music
-    steam
-    plex-desktop
-    remmina
-    # bitwarden-cli
-    bitwarden-desktop
-    discord
-    # anydesk
-    gpu-viewer
-  ];
+    packages = with pkgs; [
+      # ytmdesktop
+      youtube-music
+      steam
+      plex-desktop
+      remmina
+      # bitwarden-cli
+      bitwarden-desktop
+      discord
+      # anydesk
+      gpu-viewer
+    ];
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "25.05";
+    file = {
+      # 4 monitor setup
+      ".config/autorandr/ganoslal-4-monitor/config" = {
+        source = configLib.relativeToRoot "home/gig/common/resources/autorandr/ganoslal/ganoslal-4-monitor/config";
+        executable = true;
+      };
+      ".config/autorandr/ganoslal-4-monitor/setup" = {
+        source = configLib.relativeToRoot "home/gig/common/resources/autorandr/ganoslal/ganoslal-4-monitor/setup";
+        executable = true;
+      };
+
+      #only ultrawide setup
+      ".config/autorandr/ganoslal-only-ultra/config" = {
+        source = configLib.relativeToRoot "home/gig/common/resources/autorandr/ganoslal/ganoslal-only-ultra/config";
+        executable = true;
+      };
+      ".config/autorandr/ganoslal-only-ultra/setup" = {
+        source = configLib.relativeToRoot "home/gig/common/resources/autorandr/ganoslal/ganoslal-only-ultra/setup";
+        executable = true;
+      };
+    };
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "25.05";
+  };
 }
