ac8019de9203869291f5a3e9dbac2ec07d3fbfb4
Maxwell Rupp <maxwell@giglab.dev>
2026-04-10 20:53:33 -0400

    Revert "tried to configure greeter"
    This reverts commit 7579ee2baceca517c7eb07e69d52aa96cbb28212.

--- hosts/common/optional/bspwm.nix
+++ hosts/common/optional/bspwm.nix
@@ -6,15 +6,13 @@
       # Using LightDM instead of ly
       ly.enable = false;
 
-      # Enable auto-login for user gig
-      # Note: This will auto-login every time, even after logout
-      # To change sessions, you'll need to select a different session before logging out
+      # Auto-login configuration for LightDM
       autoLogin = {
         enable = true;
         user = "gig";
       };
 
-      # Default session for auto-login
+      # Default session (user can still select others at logout)
       defaultSession = "none+bspwm";
     };
 
@@ -27,31 +25,8 @@
         enable = true;
         greeter.enable = true;
 
-        # Configure LightDM behavior
-        extraConfig = ''
-          [Seat:*]
-          greeter-hide-users = false
-          greeter-show-manual-login = false
-          allow-guest = false
-          # Timeout before autologin (0 = immediate)
-          autologin-user-timeout = 5
-        '';
-
-        # GTK Greeter configuration
-        greeters.gtk = {
-          enable = true;
-          extraConfig = ''
-            [greeter]
-            # Pre-populate user 'gig' in the greeter
-            default-user-image = /home/gig/.face
-            hide-user-image = false
-            # Show session selector and power menu
-            show-indicators = ~session;~power
-            show-clock = true
-            # Set active monitor for multi-monitor setups
-            active-monitor = 0
-          '';
-        };
+        # LightDM auto-login allows logout to switch users/sessions
+        # The greeter will show when you explicitly log out
       };
     };
   };
@@ -69,7 +44,6 @@
   ];
 
   # Create a terminal-only session option
-  # Access this by logging out and selecting "Terminal Session" from LightDM's session menu
   services.xserver.displayManager.sessionPackages =
     let
       terminalSession = pkgs.writeTextFile {
