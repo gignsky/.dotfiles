4f2f06cf9e6cfe55e20197dc0f623724854c6ceb
Maxwell Rupp <maxwell@giglab.dev>
2026-04-20 19:14:57 -0400

    added auto-upgrade for testing

---
+++ hosts/common/core/auto-update.nix
@@ -0,0 +1,29 @@
+{ inputs, ... }:
+{
+  system.autoUpgrade = {
+    enable = true;
+    runGarbageCollection = true;
+    # randomizedDelaySec = "30min";
+    randomizedDelaySec = "0min";
+    allowReboot = false;
+    #   rebootWindow = {
+    #   lower = "03:30";
+    #   upper = "05:55";
+    # };
+    operation = "switch"; # change to "boot" if enabling reboot
+
+    # how often to update
+    # dates = "daily";
+    dates = "19:25";
+
+    # flake = "github:gignsky/.dotfiles";
+    flake = inputs.self.outPath;
+    flags = [
+      # "--update-input"
+      "--upgrade-all"
+      # "nixpkgs"
+      "--commit-lock-file"
+      "-L" # print build logs
+    ];
+  };
+}
