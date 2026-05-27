bedb41b56c1906165c3fb766ec15d449a432dac3
Maxwell Rupp <maxwell@giglab.dev>
2026-05-27 14:17:04 -0400

    change nixpkgs to stable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     #################### Official NixOS and HM Package Sources ####################
     # Stable
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     # Unstable
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Local
