1979b005fba10000b7388261d4ffa7f7ccf2fa06
Maxwell Rupp <maxwell@giglab.dev>
2026-05-30 23:14:42 -0400

    changed to develop branch

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     #################### Official NixOS and HM Package Sources ####################
     gigpkgs = {
-      url = "github:gignsky/gigpkgs";
+      url = "github:gignsky/gigpkgs/develop";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
