25f56217b57d9b851d51b20ae570b1895b629ad1
Maxwell Rupp <maxwell@giglab.dev>
2026-06-02 17:10:57 -0400

    changed follows

--- flake.nix
+++ flake.nix
@@ -6,8 +6,7 @@
     # Stable
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     gigpkgs.url = "github:gignsky/gigpkgs";
-    nixpkgs.follows = "nixpkgs-stable";
-    nixpkgs-stable.follows = "gigpkgs/nixos-stable";
+    nixpkgs.follows = "gigpkgs/nixos-stable";
     # Unstable
     nixpkgs-unstable.follows = "gigpkgs/nixos-unstable";
     # Local
@@ -19,7 +18,7 @@
     # wsl stuff
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # nixos-hardware, to fix hardware issues and firmware for specific machines
@@ -30,20 +29,20 @@
     # treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Secrets management
     sops-nix = {
       url = "github:mic92/sops-nix/master";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Pre-commit hooks for managing Git hooks declaratively
     pre-commit-hooks = {
       # url = "github:cachix/git-hooks.nix/46d55f0aeb1d567a78223e69729734f3dca25a85";
       url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-cli = {
@@ -111,7 +110,7 @@
     vscode-server = {
       url = "github:nix-community/nixos-vscode-server";
       inputs = {
-        nixpkgs.follows = "nixpkgs-stable";
+        nixpkgs.follows = "nixpkgs";
         # flake-utils.follows = "flake-utils";
       };
     };
