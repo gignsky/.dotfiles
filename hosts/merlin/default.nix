eb814323b44928d5bef66ebc89088c7a67a14065
Maxwell Rupp <maxwell@giglab.dev>
2026-06-04 00:57:29 -0400

    removed nix settings from hosts

--- hosts/merlin/default.nix
+++ hosts/merlin/default.nix
@@ -1,9 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
-  inputs,
-  lib,
-  config,
   configLib,
   ...
 }:
@@ -104,27 +101,6 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  nix =
-    let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in
-    {
-      settings = {
-        # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
-        # Opinionated: disable global registry
-        flake-registry = "";
-        # Workaround for https://github.com/NixOS/nix/issues/9574
-        nix-path = config.nix.nixPath;
-      };
-      # Opinionated: disable channels
-      channel.enable = false;
-
-      # Opinionated: make flake registry and nix path match flake inputs
-      registry = lib.mkForce (lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs);
-      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    };
 
   # fileSystems = {
   #   "/" = {
