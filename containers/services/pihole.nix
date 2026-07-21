39c5911a0534cb2fcdfade03337f632d6242c744
Maxwell Rupp <gig@gignsky.com>
2026-07-21 17:18:30 -0400

    hotfix: setting set wrong

--- containers/services/pihole.nix
+++ containers/services/pihole.nix
@@ -15,7 +15,7 @@ in
 {
   services.resolved = {
     enable = true;
-    settings."DNSStubListener" = "no";
+    settings.Resolve.DNSStubListener = "no";
   };
   systemd.tmpfiles.rules = [
     "d /var/lib/pihole/etc-pihole 0755 root root -"
