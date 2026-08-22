a96174b82d4f6edc7d61fd21efb38c61fe93e679
Maxwell Rupp <maxwell@giglab.dev>
2026-08-22 14:00:20 -0400

    cp nixpkgs-local

--- flake.nix
+++ flake.nix
@@ -11,6 +11,8 @@
     nixpkgs.url = "git+file:///home/gig/local_repos/gigpkgs";
     # Unstable
     nixpkgs-unstable.url = "github:gignsky/gigpkgs/gigos-unstable";
+    # Local
+    # nixpkgs-local.url = "git+file:///home/gig/local_repos/gigpkgs";
 
     # Home manager
     home-manager.follows = "nixpkgs/home-manager";
